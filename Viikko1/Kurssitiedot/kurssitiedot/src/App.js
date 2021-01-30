d2428ef8327f0b85ee51f49b8b886506ef26ef96
Joel <joel.ljungqvist@hotmail.com>
2021-01-30 18:55:18 +0200

    nyt se on oikeilla specseillä 1.4

--- Viikko1/Kurssitiedot/kurssitiedot/src/App.js
+++ Viikko1/Kurssitiedot/kurssitiedot/src/App.js
@@ -21,9 +21,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-        <Part part={props.part1} exercises={props.exercises1} />
-        <Part part={props.part2} exercises={props.exercises2} />
-        <Part part={props.part3} exercises={props.exercises3} />
+        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
@@ -32,7 +32,7 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        {props.total}
+        {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
       </p>
     </div>
   )
@@ -58,10 +58,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={parts[0].name} exercises1={parts[0].exercises}
-       part2={parts[1].name} exercises2={parts[1].exercises}
-       part3={parts[2].name} exercises3={parts[2].exercises}/>
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
