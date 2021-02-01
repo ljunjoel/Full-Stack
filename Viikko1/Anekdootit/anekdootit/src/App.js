f0b4910cfceee83725ac94e6cc4b4d0e35879eb1
Joel <joel.ljungqvist@hotmail.com>
2021-02-01 11:48:35 +0200

    1.12

---
+++ Viikko1/Anekdootit/anekdootit/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+const Button = (props) => {
+  return (
+    <div>
+     <button onClick={props.handleClick}>
+     {props.text}
+      </button>
+    </div>
+  )  
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const handleRandomClick = () => {
+    setSelected(Math.floor(Math.random() * 6))
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <Button handleClick={handleRandomClick} text='Random Anecdote' />
+    </div>
+  )
+}
+
+export default App
